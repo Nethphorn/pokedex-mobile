af38ff2ae68f5c279d8d4714527f0694d2c18463
Tepbrathna <nethphorn.tb@gmail.com>
2026-02-09 16:23:38 +0700

    implement fetch data from API

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,64 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, Text, View } from "react-native";
+import { Image } from "expo-image";
+
+interface Pokemon {
+  name: string;
+  url: string;
+  image: string;
+  id: number;
+  imageBack: string;
+  types: {
+    type: {
+      name: string;
+    };
+  }[];
+}
 
 export default function Index() {
+  const [pokemonData, setPokemonData] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    fetchPokemonData();
+  }, [])
+
+  async function fetchPokemonData() {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
+    const data = await response.json();
+    
+    const detailedPokemonData = await Promise.all(
+      data.results.map(async (pokemon: any) => {
+        const response = await fetch(pokemon.url);
+        const details = await response.json();
+        return {
+          name: pokemon.name,
+          url: pokemon.url,
+          image: details.sprites.front_default,
+          id: details.id,
+          imageBack: details.sprites.back_default,
+          types: details.types,
+        }
+      })
+    );
+
+    setPokemonData(detailedPokemonData);
+  }
+
+
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+   <ScrollView>
+    {pokemonData.map((pokemon) => (
+      <View key={pokemon.name}>
+
+        <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center", padding: 10 }}>
+          <Text>{pokemon.name}</Text>
+          <Text>{pokemon.types[0].type.name}</Text>
+          <Image source={{ uri: pokemon.image }} style={{ width: 100, height: 100 }} />
+          <Image source={{ uri: pokemon.imageBack }} style={{ width: 100, height: 100 }} />
+        </View>
+      </View>
+    ))}
+   </ScrollView>
   );
 }
